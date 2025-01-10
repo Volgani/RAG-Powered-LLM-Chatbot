263a0da98a006d52c5bbd1fa129f927c881b2716
Volgani <80358866+Volgani@users.noreply.github.com>
2025-01-10 12:47:38 +0100

    Update chatbot_streamlit_combined.py

--- chatbot_streamlit_combined.py
+++ chatbot_streamlit_combined.py
@@ -62,7 +62,7 @@ def display_chatbot_page():
                 text = st.text_input("Hugging Face Token (No need to insert)", type='password',value= f"{'*' * len(os.getenv('API_KEY'))}")
 
             with row_1[1]:
-                llm_model = st.text_input("LLM model", value="andreabac3/Fauno-Italian-LLM-7B")
+                llm_model = st.text_input("LLM model", value="raicrits/Hermes7b_ITA")
 
             with row_1[2]:
                 instruct_embeddings = st.text_input("Instruct Embeddings", value="nickprock/sentence-bert-base-italian-uncased")
