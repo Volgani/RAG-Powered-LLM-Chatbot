64cff0df0cddbcaac737c8d71cb822adbb53567a
Volgani <80358866+Volgani@users.noreply.github.com>
2025-01-10 11:42:43 +0100

    Update chatbot_streamlit_combined.py

--- chatbot_streamlit_combined.py
+++ chatbot_streamlit_combined.py
@@ -65,7 +65,7 @@ def display_chatbot_page():
                 llm_model = st.text_input("LLM model", value="tiiuae/falcon-7b-instruct")
 
             with row_1[2]:
-                instruct_embeddings = st.text_input("Instruct Embeddings", value="GroNLP/gpt2-small-italian-embeddings")
+                instruct_embeddings = st.text_input("Instruct Embeddings", value="dbmdz/bert-base-italian-cased")
 
             row_2 = st.columns(3)
             with row_2[0]:
@@ -146,7 +146,7 @@ def display_document_embedding_page():
         row_1 = st.columns([2, 1, 1])
         with row_1[0]:
             instruct_embeddings = st.text_input(
-                "Model Name of the Instruct Embeddings", value="GroNLP/gpt2-small-italian-embeddings"
+                "Model Name of the Instruct Embeddings", value="dbmdz/bert-base-italian-cased"
             )
         
         with row_1[1]:
