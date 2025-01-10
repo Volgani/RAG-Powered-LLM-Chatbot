af7d31b83054caa36af984e7079bfc08fc2756ef
Volgani <80358866+Volgani@users.noreply.github.com>
2025-01-10 16:14:16 +0100

    Update falcon.py

--- falcon.py
+++ falcon.py
@@ -78,7 +78,7 @@ def prepare_rag_llm(
         f"vector store/{vector_store_list}", instructor_embeddings, allow_dangerous_deserialization=True
     )
     llm = HuggingFaceHub(
-        repo_id = 'iGeniusAI/Italia-9B-Instruct-v0.1',
+        repo_id = 'ssmits/Falcon2-8B-Italian',
         model_kwargs={"temperature": temperature, "max_length": max_length},
         huggingfacehub_api_token=token
     )
