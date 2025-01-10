e1fc2f79f63918512958c79f37a2b88e1f21417b
Volgani <80358866+Volgani@users.noreply.github.com>
2025-01-10 12:05:48 +0100

    Update chatbot_streamlit_combined.py

--- chatbot_streamlit_combined.py
+++ chatbot_streamlit_combined.py
@@ -146,7 +146,7 @@ def display_document_embedding_page():
         row_1 = st.columns([2, 1, 1])
         with row_1[0]:
             instruct_embeddings = st.text_input(
-                "Model Name of the Instruct Embeddings", value="GroNLP/gpt2-small-italian-embeddings"
+                "Model Name of the Instruct Embeddings", value="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
             )
         
         with row_1[1]:
